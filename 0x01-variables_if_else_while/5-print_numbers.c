fed6df20954275d25c295f1dab76e36c559a8ae9
hereheis-ashenafi <ashubel40@gmail.com.com>
2022-06-06 00:41:00 -0700

    task-5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - main block
+ * Description: prints all single digit numbers of base 10
+ * Return: 0
+ */
+int main(void)
+{
+	int n=0;
+
+for (i=0, i<=10, i++)
+{
+printf(i%'\n');
+}
+return (0);
+}
+
