3e8371a5ba0760af3c2d231b2982c0c2670e2aea
hereheis-ashenafi <ashubel40@gmail.com.com>
2022-06-23 13:30:41 -0700

    task-8_adv

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,73 +1,49 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * wildcmp - check the code for Holberton School students.
- * @s1: string
- * @s2: string
- * Return: Always 0.
+ * check - Checker for the palindrome
+ *
+ * @s: String
+ *
+ * @start: int moves from right to left
+ *
+ * @end: int move from left to right
+ *
+ * @pair: int
+ *
+ * Return: 0 or 1
  */
 
-int wildcmp(char *s1, char *s2)
+int check(char *s, int start, int end, int pair)
 {
-return (checker(s1, s2, 0, 0, -1));
-}
-
-/**
- * checker - helper
- * @s1: string
- * @s2: string
- * @a: int
- * @b: int
- * @wildUsed: int
- * Return: Always 0.
- */
-int checker(char *s1, char *s2, int a, int b, int wildUsed)
-{
-
-//printf("s1 <%c> a <%i> s2 <%c> b <%i> \n", s1[a], a, s2[b], b);
-
-if (s1[a] != '\0')
-{	
-	if (s2[b] == '\0')
+	if ((start == end && pair != 0) || (start == end + 1 && pair == 0))
+		return (1);
+	else if (s[start] != s[end])
 		return (0);
-	else if (s2[b] == '*')
-	{
-		if (s2[b + 1] == '*'){
-			//printf("Pass 1\n");
-			return (checker(s1, s2, a, b + 1, b));
-		}
-		else if (s2[b + 1] == s1[a]){
-			//printf("Pass 2\n");
-			return (checker(s1, s2, a, b + 1, b));
-		}	
-		else if (s1[a + 1] != s2[b + 1]){
-			//printf("Pass 3\n");
-			return (checker(s1, s2, a + 1, b, b));	
-		}
-		else if (s1[a + 1] == s2[b + 1]){
-			//printf("Pass 3\n");
-			return (checker(s1, s2, a + 1, b + 1, b));	
-		}
-		
-	}
-	else if ((s1[a] == s2[b]) || (s2[b] == '*' && s2[b + 1] == s1[a + 1])){
-		//printf("Pass 4\n");
-		return (checker(s1, s2, a + 1, b + 1, wildUsed));
-	}
 	else
-	{
-		if (wildUsed == -1)
-		{
-			//printf("No wild card\n");
-			return (0);
-		}
-		//printf("Back to wildcard\n");
-		return (checker(s1, s2, a, wildUsed, wildUsed));
-	}
+		return (check(s, start + 1, end - 1, pair));
 }
-if (s2[b] != '\0')
-	return 0;
 
-return (1);
+/**
+ * last_index - Returns the last index of a string (counts the null char)
+ *
+ * @s: Pointer the string
+ *
+ * Return: int
+ */
+
+int last_index(char *s)
+{
+	int n = 0;
+
+	if (*s > '\0')
+		n += last_index(s + 1) + 1;
+
+	return (n);
+}
+
+/**
+ * is_palindrome - Check if a string is a palindrome
+ *
+ * @s: String to check
 
