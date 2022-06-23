04160a969af1435334c25d7d60f97bfea7c1651c
hereheis-ashenafi <ashubel40@gmail.com.com>
2022-06-23 12:56:10 -0700

    task-4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "mainn.h"
+
+/**
+ * factorial - finds factorial
+ * @n: int
+ * Return: int
+ */
+
+int factorial(int n)
+{
+
+if (n < 0)
+{
+	return (-1);
+}
+else if (n == 0)
+{
+	return (1);
+}
+
+return (n * factorial(n - 1));
+
+}
