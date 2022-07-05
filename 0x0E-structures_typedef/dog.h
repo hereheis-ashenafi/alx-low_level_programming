286959293187c9125506ac5ac53ab439c9c374c2
hereheis-ashenafi <ashubel40@gmail.com.com>
2022-07-05 03:31:49 -0700

    for-dog-h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,16 +1,24 @@
-#include <stdio.h>
+#ifndef DOG_H
+#define DOG_H
 
-struct dog
+/**
+ * struct dog - structure definition of a dog
+ * @name: character string
+ * @age: integer
+ * @owner: character string
+ */
+
+typedef struct dog
 {
-    char *name;
-    float age;
-    char *owner;
-};
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
 
-int main(void)
-{
-    struct dog my_dog;
-
-    return (0);
-
-}
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+#endif
